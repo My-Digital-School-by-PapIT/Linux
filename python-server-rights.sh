5104ef7a8fbad03426ea13fad078861e169035cd
chrichri <christophe.brun@papit.fr>
2024-12-14 00:52:40 +0100

    FIX typo.

--- python-server-rights.sh
+++ python-server-rights.sh
@@ -5,22 +5,22 @@ groupadd webgroup
 
 # Créer un utilisateur développeur avec les droits adéquats
 useradd -m -G webgroup devuser
-mkdir -p /var/www/html/digicomp
-touch /var/www/html/digicomp/index.html
-chown -R devuser:webgroup /var/www/html/digicomp
-chmod -R 770 /var/www/html/digicomp
+mkdir -p /var/www/html/mds
+touch /var/www/html/mds/index.html
+chown -R devuser:webgroup /var/www/html/mds
+chmod -R 770 /var/www/html/mds
 
 # Créer un utilisateur production avec les droits adéquats
 useradd -m -G webgroup produser
-chown -R produser:webgroup /var/www/html/digicomp
-chmod -R 750 /var/www/html/digicomp
+chown -R produser:webgroup /var/www/html/mds
+chmod -R 750 /var/www/html/mds
 
 # Script pour lancer le serveur Python
 cat << 'EOF' > /usr/local/bin/start_server.sh
 #!/bin/bash
 # Run as produser
 su produser << 'EOF2'
-python3 -m http.server -d /var/www/html/digicomp/
+python3 -m http.server -d /var/www/html/mds/
 EOF
 
 # Rendre le script exécutable
